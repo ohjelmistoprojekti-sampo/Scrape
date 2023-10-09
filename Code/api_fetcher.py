19dab920e84f682ed54b9132bdc7a7dde13a7326
bgx160 <122889193+bgx160@users.noreply.github.com>
2023-10-09 12:26:58 +0000

    Add functionality to fetch items from API based on category id

--- Code/api_fetcher.py
+++ Code/api_fetcher.py
@@ -2,27 +2,47 @@ import requests
 
 API_URL = 'https://api.huuto.net/1.1/'
 
+def make_api_request(endpoint, params=None, timeout=5):
+    try:
+        response = requests.get(f"{API_URL}{endpoint}", params=params, timeout=timeout)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as exception:
+        print(f"Error making API request: {exception}")
+        return None
+
 def fetch_by_keyword(keyword, timeout=5):
-    response = requests.get(f"{API_URL}items?words={keyword}", timeout=timeout)
+    params = {"words": keyword}
+    data = make_api_request("items", params=params, timeout=timeout)
+    if data:
+        items = data.get("items", [])
+        return clean_data(items)
+    return []
 
-    if response.status_code == 200:
-        data = response.json()
-
-    items = data["items"]
-    return clean_data(items)
+def fetch_items_by_cat_id(id, limit=500, timeout=5):
+    params = {"limit": limit}
+    data = make_api_request(f"categories/{id}/items", params=params, timeout=timeout)
+    if data:
+        items = data.get("items", [])
+        return clean_data(items)
+    return []
 
 def clean_data(data):
     items_clean = []
     for item in data:
         clean_item = {
-            "title": item["title"],
-            "buy_now_price": item["buyNowPrice"],
-            "location": item["location"],
-            "postal_code": item["postalCode"],
-            "image": item["images"][0].get("links").get("medium")
+            "title": item.get("title", ""),
+            "buy_now_price": item.get("buyNowPrice", 0),
+            "location": item.get("location", ""),
+            "postal_code": item.get("postalCode", ""),
+            "current_price": item.get("currentPrice", 0)
         }
+        if item.get("images"):
+            clean_item["image"] = item["images"][0].get("links", {}).get("medium", "")
         items_clean.append(clean_item)
     return items_clean
 
 item_data = fetch_by_keyword("aaltomaljakko")
-print(item_data)
+furniture_data = fetch_items_by_cat_id(339)
+
+print(furniture_data)
