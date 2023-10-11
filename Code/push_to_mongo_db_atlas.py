ba74d964260e3e396578628a620d7615bd6faec0
Topias <you@example.com>
2023-10-11 15:17:44 +0300

    Add function to push data in mongoDB

--- Code/push_to_mongo_db_atlas.py
+++ Code/push_to_mongo_db_atlas.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 from api_fetcher import fetch_by_keyword
 
-connection = "mongodb+srv://admin:qFO3WyPgMyrEIP2g@sampodb.mq9vg8p.mongodb.net/"
+connection = "mongodb+srv://admin:password@sampodb.mq9vg8p.mongodb.net/" # define password
 
 client = MongoClient(connection)
 
@@ -13,4 +13,4 @@ data = fetch_by_keyword("aalto maljakko", 50)[0]
 
 print(data)
 
-collection.insert_one(data)
+# collection.insert_one(data)
