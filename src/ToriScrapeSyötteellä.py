d9f3cf8649bf57f3504d546a3c16b595f90875f8
Leo Lehto <leo.lehto4@gmail.com>
2023-11-22 14:23:39 +0200

    scrape 200 pages from tori

--- src/ToriScrapeSyötteellä.py
+++ src/ToriScrapeSyötteellä.py
@@ -35,7 +35,7 @@ def cond(link):
     return(cond_value)
     
 
-def ScrapeTori(num_pages=1):  # Set the number of pages you want to scrape
+def ScrapeTori(num_pages=200):  # Set the number of pages you want to scrape
     # Set up Chrome driver
     driver = webdriver.Chrome()
 
@@ -43,12 +43,12 @@ def ScrapeTori(num_pages=1):  # Set the number of pages you want to scrape
 
     for page_number in range(1, num_pages + 1):
         #change the url based on products wanted
-        URL = f"https://www.tori.fi/koko_suomi/sisustus_ja_huonekalut/hyllyt_ja_sailytys?ca=18&q=sisustus%20ja%20huonekalut&cg=3020&st=s&c=3022&f=p&w=3&o={page_number}"
+        URL = f"https://www.tori.fi/koko_suomi/sisustus_ja_huonekalut?ca=18&q=sisustus%20ja%20huonekalut&cg=3020&st=s&w=3&o={page_number}"
         driver.get(URL)
         print(URL)
 
         try:
-            WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'item_row_flex')))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'item_row_flex')))
         except TimeoutException:
             print(f"Timeout waiting for page {page_number} to load")
             continue  # Move on to the next page if the current one times out
@@ -63,10 +63,17 @@ def ScrapeTori(num_pages=1):  # Set the number of pages you want to scrape
         for i, products in enumerate(products, start=1):
             try:
                 title = products.find('div', class_="li-title").contents[0]
-                price = products.find('p', class_="list_price ineuros").contents[0].replace(" ", "")
+                try:
+                    price = products.find('p', class_="list_price ineuros").contents[0].replace(" ", "")
+                except AttributeError:
+                    price = None
                 product_url = products.get('href')
 
-                # Get the url for product image
+                #remove non digits from price and convert to int
+                cleaned_price = ''.join(p for p in price if p.isdigit())
+                price_int = int(cleaned_price)
+                
+                #find if product has image url
                 image_container = products.find('div', class_='image_container')
                 image_div = image_container.find('div', class_='item_image_div') if image_container else None
                 img_url = image_div.find('img', class_='item_image')['src'] if image_div and image_div.find('img', class_='item_image') else 'N/A'
@@ -74,27 +81,27 @@ def ScrapeTori(num_pages=1):  # Set the number of pages you want to scrape
                 # Get the product condition
                 condition = cond(product_url)
                 condition_number = translate_condition_to_number(condition)
-
+                if price is not None:
                 # Create a dictionary for each item
-                item_data = {
-                    "title": f"{title}",
-                    "price": price,
-                    "imageurl": img_url,
-                    "condition": condition_number
-                }
-                scraped_data.append(item_data)
+                    item_data = {
+                        "title": title,
+                        "price": price_int,
+                        "imageurl": img_url,
+                        "condition": condition_number
+                    }
+                    scraped_data.append(item_data)
 
                 #follow the process
                 print(f"Page {page_number}, Item {i} - title: {item_data['title']}, price: {item_data['price']}, imageurl: {item_data['imageurl']}, condition: {item_data['condition']}")
 
             except Exception as e:
                 print(f"Error processing item {i} on page {page_number}: {e}")
-        #check amount of product scraped
+        #check amount of products scraped
         print(f'Page {page_number} - listings: {len(scraped_data)}')
-
+    
     driver.quit()  # Close the browser when done
-    return scraped_data
+    return scraped_data 
+    
 
-# Call the function to execute the scraping and display JSON
-scraped_data = ScrapeTori(num_pages=1)  # You can adjust the number of pages as needed
-print(scraped_data)
+if __name__ == "__main__":
+    ScrapeTori()
--- src/main.py
+++ src/main.py
@@ -2,13 +2,16 @@ from api_fetcher import fetch_by_keyword, fetch_items_by_cat_id
 from push_to_mongo_db_atlas import push_many_to_mongo_db, push_one_to_mongo_db
 from IkeaScrapeCategoryAllTheProducts import scrape_ikea
 from HuutoNet_Scrape import scrape_huuto
+from ToriScrapeSyötteellä import ScrapeTori
 
 
 if __name__ == "__main__":
     huuto_base_url = "https://www.huuto.net/haku/category/339"
     huuto_data = scrape_huuto(huuto_base_url)
     ikea_data = scrape_ikea()
+    tori_data = ScrapeTori()
     
     push_many_to_mongo_db(huuto_data)
     push_many_to_mongo_db(ikea_data)
+    push_many_to_mongo_db(tori_data)
     
